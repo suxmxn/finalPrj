4ba11b358c51fb6462816c43feea232686d0b801
Su-Min Lee <soomin1938@daum.net>
2021-12-05 04:22:19 +0900

    Product 모델을 생성하고 시간 설정하기

--- shopping/models.py
+++ shopping/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=30)  # 상품명
+    content = models.TextField()    # 설명
+    price = models.CharField(max_length=20) # 상품 가격
+    maker = models.CharField(max_length=30) # 제조사
+    made_at = models.DateTimeField()    # 제조년월
+    color = models.CharField(max_length=20) # 컬러
+
+    # product_image  # 상품 이미지
+    # category  # 카테고리
+
+    def __str__(self):
+        return f'[{self.pk}]{self.name}'
