0fe416799617c247383ea7a8d50c6c62c7116d9b
Su-Min Lee <soomin1938@daum.net>
2021-12-18 18:30:47 +0900

    포스트 목록 페이지에 카테고리 기능 추가하기

--- shopping/models.py
+++ shopping/models.py
@@ -1,7 +1,18 @@
 from django.db import models
 from django.contrib.auth.models import User
+import os
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    slug = models.SlugField(max_length=200, unique=True, allow_unicode=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
 class Product(models.Model):
     name = models.CharField(max_length=30)  # 상품명
     content = models.TextField()    # 설명
@@ -13,7 +24,7 @@ class Product(models.Model):
 
     author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
     product_image = models.ImageField(upload_to='shopping/images/%Y/%m/%d/', blank=True)  # 상품 이미지
-    # category  # 카테고리
+    category = models.ForeignKey(Category, null=True, blank=True, on_delete=models.SET_NULL)  # 카테고리
 
     def __str__(self):
         return f'[{self.pk}]{self.name} :: {self.author}'
--- shopping/views.py
+++ shopping/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
-from .models import Product
+from .models import Product, Category
 
 class ProductList(ListView):
     model = Product
     ordering = '-pk'
+
+    def get_context_data(self, **kwargs):
+        context = super(ProductList, self).get_context_data()
+        context['categories'] = Category.objects.all()
+        context['no_category_product_count'] = Product.objects.filter(category=None).count()
+        return context
 
 class ProductDetail(DetailView):
     model = Product
